8ffcef7e2c0c8160754b6ab045f6516566cb3737
Paul Ehigie <ehigiepaul@yahoo.com>
2018-05-31 17:16:43 +0100

    menu done

---
+++ www/js/core.js
@@ -0,0 +1,10 @@
+var menu = document.querySelector('.menu')
+var menuBox = document.querySelector('.menuSide')
+var tm = 0
+
+function menuToggle() {
+
+    menu.classList.toggle('trans')
+    menuBox.classList.toggle('trans')
+
+}
